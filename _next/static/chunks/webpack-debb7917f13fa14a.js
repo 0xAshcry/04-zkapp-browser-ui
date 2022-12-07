7ac2bb12f3588fbb22020701978a4d64422e92e3
root <root@localhost.localdomain>
2022-12-07 21:43:35 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-debb7917f13fa14a.js
+++ _next/static/chunks/webpack-debb7917f13fa14a.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "39a9c7cc56f0f6e0" + ".css";
+/******/ 			return "static/css/" + "06c9ad9e2c78853b" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
